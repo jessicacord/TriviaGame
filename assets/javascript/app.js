5fb4fdc03d0cb15e46076ea00e36adabc97fc1ce
jessicacord <jessicaleecord@gmail.com>
2018-01-19 20:57:38 -0500

    added styles, images, and adjusted timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,73 +12,73 @@ var triviaGame = {
             question: "Every Rose Has Its Thorn",
             correctAnswer: "Poison",
             answers: ["Guns N' Roses","Bon Jovi","Stryper","Poison"],
-            image: ""
+            image: "assets/images/poison.jpg"
         },
         {
             question: "What About Love",
             correctAnswer: "Heart",
             answers: ["Fleetwood Mac","Motley Crue","Heart","Journey"],
-            image: ""
+            image: "assets/images/heart.jpg"
         },
         {
             question: "Love Bites",
             correctAnswer: "Def Leppard",
             answers: ["AC/DC","Aerosmith","Def Leppard","Van Halen"],
-            image: ""
+            image: "assets/images/def-leppard.jpg"
         },
         {
             question: "Nothing's Gonna Stop Us Now",
             correctAnswer: "Jefferson Starship",
             answers: ["Journey","Jefferson Starship","Queen","Cinderella"],
-            image: ""
+            image: "assets/images/jefferson-starship.jpg"
         },
         {
             question: "Is This Love",
             correctAnswer: "Whitesnake",
             answers: ["Whitesnake","Skid Row","Duran Duran","Genesis"],
-            image: ""
+            image: "assets/images/whitesnake.jpg"
         },
         {
             question: "I'll Be There For You",
             correctAnswer: "Bon Jovi",
             answers: ["Rush","Heart","Bon Jovi","Warrant"],
-            image: ""
+            image: "assets/images/bon-jovi.jpg"
         },
         {
             question: "Don't Stop Believin'",
             correctAnswer: "Journey",
             answers: ["Aerosmith","David Bowie","Bon Jovi","Journey"],
-            image: ""
+            image: "assets/images/journey.jpg"
         },
         {
             question: "Total Eclipse of the Heart",
             correctAnswer: "Bonnie Tyler",
             answers: ["Cher","Whitney Houston","Bonnie Tyler","Stevie Nicks"],
-            image: ""
+            image: "assets/images/bonnie-tyler.jpg"
         },
         {
             question: "Faithfully",
             correctAnswer: "Journey",
             answers: ["Foreigner","Steely Dan","Journey","U2"],
-            image: ""
+            image: "assets/images/journey.jpg"
         },
         {
             question: "Heaven",
             correctAnswer: "Bryan Adams",
             answers: ["Bruce Springsteen","Michael Bolton","Bryan Adams","Phil Collins"],
-            image: ""
+            image: "assets/images/bryan-adams.jpg"
         },
         {
             question: "Reason To Live",
             correctAnswer: "KISS",
             answers: ["Twisted Sister","Van Halen","KISS","Quiet Riot"],
-            image: ""
+            image: "assets/images/kiss.jpg"
         },
         {
             question: "I Want To Know What Love Is",
             correctAnswer: "Foreigner",
             answers: ["Rush","The Police","Foreigner","Boston"],
-            image: ""
+            image: "assets/images/foreigner.jpg"
         }
 
     ],
@@ -88,7 +88,7 @@ var triviaGame = {
     incorrect : 0,
     unanswered : 0,
     questionNumber : 0,
-    timer: 5,
+    timer: 10,
     answeredCorrect : false,
 
     //Start game function
@@ -101,7 +101,7 @@ var triviaGame = {
 
     //Start a timer
     startTimer : function() {
-        $("#timer").html("<H3>").text(triviaGame.timer);
+        $("#timer").html($("<H3>").text(triviaGame.timer));
         if (!clockRunning) {
             intervalId = setInterval(triviaGame.count, 1000);
             clockRunning = true;
@@ -113,7 +113,7 @@ var triviaGame = {
 
         if ( triviaGame.timer > 1 ) {
             triviaGame.timer--;
-            $("#timer").html("<H3>").text(triviaGame.timer);
+            $("#timer").html($("<H3>").text(triviaGame.timer));
         } else {
             triviaGame.answeredCorrect = false;
             triviaGame.stopTimer();
@@ -130,7 +130,7 @@ var triviaGame = {
         
         clearInterval(intervalId);
         clockRunning = false;
-        triviaGame.timer = 5;
+        triviaGame.timer = 10;
 
         triviaGame.displayAnswer();
         
@@ -139,20 +139,22 @@ var triviaGame = {
     displayAnswer : function() {
         
         if ( !triviaGame.answeredCorrect ) {
-            $("#game").html($("<H2>").text("The correct answer is:"));
-            $("#game").append($("<H4>").text(triviaGame.questions[triviaGame.questionNumber].correctAnswer));
+            $("#game").html($("<H4>").text("The correct answer is:"));
+            $("#game").append($("<H2>").text(triviaGame.questions[triviaGame.questionNumber].correctAnswer));
     
         } else {
             $("#game").html($("<H2>").text("That is correct!"));
         }
+
+        $("#game").append($("<IMG>").attr("src", triviaGame.questions[triviaGame.questionNumber].image));
         
         
         triviaGame.questionNumber+= 1;
 
         if (triviaGame.questionNumber < triviaGame.questions.length){
-            setTimeout(triviaGame.displayQuestion, 3000);
+            setTimeout(triviaGame.displayQuestion, 4000);
         } else {
-            setTimeout(triviaGame.endGame, 3000);
+            setTimeout(triviaGame.endGame, 4000);
         }
         
 
@@ -161,7 +163,7 @@ var triviaGame = {
     displayQuestion : function() {
         triviaGame.startTimer();
 
-        $("#game").html($("<H2>").text(triviaGame.questions[triviaGame.questionNumber].question));
+        $("#game").html($("<H4>").text(triviaGame.questions[triviaGame.questionNumber].question));
 
         $("#game").append($("<UL>").attr("id", "answers"));
 
